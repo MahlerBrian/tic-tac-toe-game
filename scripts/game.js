2f39ac275867eb2de60f5d4ed6cf7d43a8f3cb34
Brian Albert <brian.david.albert@gmail.com>
2022-05-15 12:53:03 -0400

    fixed bug to deactivate game board when game is over

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,7 @@
 function restartGameStatus() {
     activePlayer = 0;
     currentRound = 1;
+    gameIsOver = false;
     gameOverElement.firstElementChild.innerHTML = 'You won, <span id="winner-name">PLAYER NAME</span>';
     gameOverElement.style.display = 'none';
 
@@ -40,7 +41,7 @@ function switchPlayer() {
 
 
 function selectGameField(event) {
-    if (event.target.tagName !== 'LI') {
+    if (event.target.tagName !== 'LI' || gameIsOver) {
         return;
     }
     
@@ -120,6 +121,7 @@ function checkForGameOver() {
 }
 
 function endGame(winnerId) {
+    gameIsOver = true;
     gameOverElement.style.display = 'block';
     
     if (winnerId > 0) {
